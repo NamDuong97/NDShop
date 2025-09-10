0c80f4b66bd972d9a5b6aac7f0a547a18df2df47
NamDuong97 <nakani3797@gmail.com>
2025-09-10 15:04:15 +0700

    khoi tao du an NDShop_Ecomerce

---
+++ NDShop_Ecomerce/NDShop_Ecomerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NDShop_Ecomerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
